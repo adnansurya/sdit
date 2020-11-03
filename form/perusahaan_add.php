6b15c39fdb46c06aa619650c424596f0cc84c65d
Adnan <muh.adnansuryaazis@gmail.com>
2020-11-04 06:20:26 +0800

    negara as uppercase letter

--- form/perusahaan_add.php
+++ form/perusahaan_add.php
@@ -5,7 +5,7 @@ if(isset($_POST['kode_usaha']) && isset($_POST['kode_badan']) && isset($_POST['n
 && isset($_POST['provinsi']) && isset($_POST['negara']) && isset($_POST['telp'])){
     $sql = "INSERT INTO perusahaan (kode_perusahaan, kode_badan, nama_perusahaan, alamat, provinsi, negara, no_telp) 
     VALUES ('".$_POST['kode_usaha']."', '".$_POST['kode_badan']."', '".$_POST['nama_usaha']."', '".$_POST['alamat']."'
-    , '".$_POST['provinsi']."', '".$_POST['negara']."', '".$_POST['telp']."')";
+    , '".$_POST['provinsi']."', '".strtoupper($_POST['negara'])."', '".$_POST['telp']."')";
     $result = mysqli_query($conn, $sql);
     if(!($result)){
         echo 'Error query perusahaan';
