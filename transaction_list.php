7437d1b3062cc189ce75bc7a813e15f885bb2d52
Muhammad Adnan Surya <muh.adnansuryaazis@gmail.com>
2020-11-19 16:26:03 +0800

    file export for transaction list

--- transaction_list.php
+++ transaction_list.php
@@ -190,11 +190,21 @@
     <script src="vendors/datatables.net/js/jquery.dataTables.min.js"></script>
     <script src="vendors/datatables.net-bs4/js/dataTables.bootstrap4.min.js"></script>
     <script src="vendors/datatables.net-buttons/js/dataTables.buttons.min.js"></script>
+    <script src="vendors/datatables.net-buttons/js/buttons.flash.min.js"></script>
+    <script src="vendors/datatables.net-buttons/js/buttons.html5.min.js"></script>
+    <script src="vendors/datatables.net-buttons/js/buttons.print.min.js"></script>
     <script src="vendors/datatables.net-buttons-bs4/js/buttons.bootstrap4.min.js"></script>
+    <script src="vendors/jszip/dist/jszip.min.js"></script>
+    <script src="vendors/pdfmake/build/pdfmake.min.js"></script>
+    <script src="vendors/pdfmake/build/vfs_fonts.js"></script>
     <script type="text/javascript" src="vendors/datatables-responsive/js/dataTables.responsive.js"></script>
     <script>
         jQuery('#datatable').DataTable({
-            "responsive" : true
+            "responsive" : true,
+            dom: 'frtipB',
+            buttons: [
+            'copy', 'csv', 'excel', 'pdf', 'print'
+            ]
         });
 
         jQuery('#detailModal').on('show.bs.modal', function (event) {
